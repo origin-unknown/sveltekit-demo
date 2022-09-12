56a410983da70a54a6953dd36cbc8e5f7bfbf0f1
origin-unknown <ich.hier.wer.dort@gmx.de>
2022-09-12 18:09:24 +0200

    Updates

---
+++ _app/immutable/chunks/0-92be9a03.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_layout.svelte-d4ae812d.js";export{t as component};
---
+++ _app/immutable/chunks/3-94ca56b5.js
@@ -0,0 +1,1 @@
+import{_ as r}from"./_page-0b0d8028.js";import{default as t}from"../components/pages/about/_page.svelte-1770d073.js";export{t as component,r as shared};
